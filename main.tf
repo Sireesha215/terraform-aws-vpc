2943eef4996531e56c59c0245b2e0ac866424cf4
Sireesha <sireesha.lakkanaboina@gmail.com>
2026-07-08 10:56:52 +0530

    commit

--- main.tf
+++ main.tf
@@ -6,9 +6,19 @@ resource "aws_vpc" "main" {
   tags = local.vpc_final_tags
 }
 
-resource "aws_internet_gateway" "main" {
-  vpc_id = aws_vpc.main.id # VPC association
+resource "aws_subnet" "public" {
+  count = length(var.public_subnet_cidrs)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.public_subnet_cidrs[count.index]
+  availability_zone = local.az_names[count.index]
+  map_public_ip_on_launch = true
 
-  tags = local.igw_final_tags
+  tags = merge(
+        local.common_tags,
+        # roboshop-dev-public-us-east-1a
+        {
+            Name = "${var.project}-${var.environment}-public-${local.az_names[count.index]}"
+        },
+        var.public_subnet_tags
+    )
 }
-
