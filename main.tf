5a2ed254b3e5dc078bbad074e4d09ce0b1cee31a
Sireesha <sireesha.lakkanaboina@gmail.com>
2026-07-08 11:57:59 +0530

    commit

--- main.tf
+++ main.tf
@@ -5,6 +5,11 @@ resource "aws_vpc" "main" {
 
   tags = local.vpc_final_tags
 }
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id # VPC association
+
+  tags = local.igw_final_tags
+}
 
 resource "aws_subnet" "public" {
   count = length(var.public_subnet_cidrs)
